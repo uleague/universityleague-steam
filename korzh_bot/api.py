0e5d1e6fa42890eb3ecb5cb5ab3df7f848d60120
Evgenii Petrovich <parapheen6@gmail.com>
2020-08-24 13:51:51 +0300

    healthckeck check

--- korzh_bot/api.py
+++ korzh_bot/api.py
@@ -14,8 +14,14 @@ async def get_ping(request):
     Heartbeat index route
     """
     client = request.app["bot"]
-    response = {"I'm": client.user.name}
-    return json_response(response, status=200, content_type="application/json")
+    if client.user.name:
+        response = {"I'm": client.user.name}
+        return json_response(response, status=200, content_type="application/json")
+    else:
+        response = {
+            "Error": "Couldn't get bot's username. Check if it is online on Steam."
+        }
+        return json_response(response, status=500, content_type="application/json")
 
 
 @routes.get("/friends")
